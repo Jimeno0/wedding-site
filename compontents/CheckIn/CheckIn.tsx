c5393df993b3b4ad0922338d0647ee1a0ce873f2
jimeno0 <jimenocontact@gmail.com>
2022-09-17 15:11:21 +0200

    add typeform

--- compontents/CheckIn/CheckIn.tsx
+++ compontents/CheckIn/CheckIn.tsx
@@ -1,11 +1,19 @@
 import { CheckInWrapper } from './CheckIn.styled';
-import { Text } from '../Text';
 
 export const CheckIn = () => {
   return (
     <CheckInWrapper>
       <a>Check in</a>
-      <Text>Ayudanos a organizarnos haciendo tu check in</Text>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfQOre1e1r1mC1-j9KnVz0E2Gddh-b4F26-QfBekq-Kd2-MGA/viewform?embedded=true"
+        width="100%"
+        height="797"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Cargando…
+      </iframe>
       <picture>
         <source srcSet="/bilma.jpg" type="image/webp" />
         <img src="/bilma.jpg" alt="fachada finca el gasto" />
