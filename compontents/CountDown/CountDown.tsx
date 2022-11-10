720643a5014803a23422986c2d02dfab5080f61b
jimeno0 <jimenocontact@gmail.com>
2022-11-10 19:12:38 +0100

    try to fix location

--- compontents/CountDown/CountDown.tsx
+++ compontents/CountDown/CountDown.tsx
@@ -45,7 +45,7 @@ export const CountDown = () => {
         }}
         target="_blank"
         rel="noreferrer"
-        href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=XzhwMTNlaDltNnAwamNiOWo4OHMzMmI5azcwcDNhYjlvOGQxamdiYTQ4NHI0MmdpNjZoMjNjaGhpNnMgNjVmODQ3ZTA2YTQyZDg3M2I4ODc1M2Q4YThkYjQ4YmQwZWQ0NGQ3ODg2OTE3MGZkZDRhNjAyODBmYTgzYTZiM0Bn&amp;tmsrc=65f847e06a42d873b88753d8a8db48bd0ed44d78869170fdd4a60280fa83a6b3%40group.calendar.google.com"
+        href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Boda%20Marta%20%26%20Carlos%20%F0%9F%92%92&dates=20230715T190000Z+01:00/20230716T050000Z+01:00&details=Toda+la+info+en+la+web:+perez-alejano.com&location=Finca%20El%20Gasco%2C%20Almte.%20Marqu%C3%A9s%20de%20Valterra%2C%206%2C%2028250%20Torrelodones%2C%20Madrid%2C%20Espa%C3%B1a&sf=true&output=xml"
       >
         Añadir al calendario
       </a>
