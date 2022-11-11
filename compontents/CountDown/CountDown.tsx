63fdef3f22292f7dddc1137c52ae906ab19f6be6
jimeno0 <jimenocontact@gmail.com>
2022-11-11 18:07:48 +0100

    use addEvent to add to calendar

--- compontents/CountDown/CountDown.tsx
+++ compontents/CountDown/CountDown.tsx
@@ -45,7 +45,7 @@ export const CountDown = () => {
         }}
         target="_blank"
         rel="noreferrer"
-        href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Boda%20Marta%20%26%20Carlos%20%F0%9F%92%92&dates=20230715T190000Z+01:00/20230716T050000Z+01:00&details=Toda+la+info+en+la+web:+perez-alejano.com&location=Finca%20El%20Gasco%2C%20Almte.%20Marqu%C3%A9s%20de%20Valterra%2C%206%2C%2028250%20Torrelodones%2C%20Madrid%2C%20Espa%C3%B1a&sf=true&output=xml"
+        href="https://www.addevent.com/event/nj15522716"
       >
         Añadir al calendario
       </a>
