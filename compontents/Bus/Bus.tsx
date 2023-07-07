9126523774dfb1d4e4ae77d1d05f41e6f16603cb
jimeno0 <jimenocontact@gmail.com>
2023-07-07 18:13:41 +0200

    remove bold stuff

--- compontents/Bus/Bus.tsx
+++ compontents/Bus/Bus.tsx
@@ -10,22 +10,22 @@ export const Bus = () => {
       </picture>
     <SideText size="1.3rem" uppercase>AUTOBUSES</SideText>
 
-      <Text size="1rem" style={{ width: '80%', marginTop: 5 }}>
+      <Text bold size="1rem" style={{ width: '80%', marginTop: 5 }}>
         IDA:
       </Text>
-      <Text bold size="1rem" styles={descriptionStyles}>
+      <Text size="1rem" styles={descriptionStyles}>
         {`- Moncloa (pº Moret - Intercambiador):     17:30h`}
       </Text>
-      <Text bold size="1rem" styles={descriptionStyles}>
+      <Text size="1rem" styles={descriptionStyles}>
         {`- Hotel PAX Torrelodones:     18:15h`}
       </Text>
       <Text size="1rem" style={{ width: '80%', marginTop: 15 }}>
         VUELTA:
       </Text>
-      <Text bold size="1rem" styles={descriptionStyles}>
+      <Text size="1rem" styles={descriptionStyles}>
         {`- Finca El Gasco (ambas paradas):     2:30h`}
       </Text>
-      <Text bold size="1rem" styles={descriptionStyles}>
+      <Text size="1rem" styles={descriptionStyles}>
         {`- Finca El Gasco (ambas paradas):     5:00h`}
       </Text>
     </CheckInWrapper>
